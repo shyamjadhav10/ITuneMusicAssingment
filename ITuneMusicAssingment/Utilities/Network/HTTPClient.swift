8e3b2f1d1a34bacd7190f7fb0bf581ca878f6e79
Shyam Jadhav <shyamsundar@wakency.com>
2020-04-23 21:22:44 +0530

    [Setup] - Added request protocol

---
+++ ITuneMusicAssingment/Utilities/Network/HTTPClient.swift
@@ -0,0 +1,11 @@
+//
+//  HTTPClient.swift
+//  ITuneMusicAssingment
+//
+//  Created by Shyam Jadhav on 23/04/20.
+//  Copyright © 2020 Shyam Jadhav. All rights reserved.
+//
+
+
+
+import Foundation
---
+++ ITuneMusicAssingment/Utilities/Protocol/RequestProtocol.swift
@@ -0,0 +1,31 @@
+//
+//  RequestProtocol.swift
+//  ITuneMusicAssingment
+//
+//  Created by Shyam Jadhav on 23/04/20.
+//  Copyright © 2020 Shyam Jadhav. All rights reserved.
+//
+
+import Foundation
+
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete  = "DELETE"
+}
+
+public typealias HTTPHeaders = [String: Any]
+
+
+protocol RequestProtocol {
+
+    var baseURL: URL { get }
+    var path: String { get }
+    var httpMethod: HTTPMethod { get }
+    var httpBody: Data? { get }
+    var headers: HTTPHeaders? { get }
+
+}
