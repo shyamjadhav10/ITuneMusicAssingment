13b12acaa33cf5d11bff1a7484d149d0df05b631
Shyam Jadhav <shyamsundar@wakency.com>
2020-04-24 12:07:54 +0530

    [View Model]  -  Added track listing api implementation in view model

--- ITuneMusicAssingment/Search Listing/ViewModel/TrackListViewModel.swift
+++ ITuneMusicAssingment/Search Listing/ViewModel/TrackListViewModel.swift
@@ -9,23 +9,48 @@
 import Foundation
 
 
-class ContactListViewModel {
+class TrackListViewModel {
     
+    // Rendering data
     let screenTitle =  "iMusic"
     
     var isLoading: Bindable<Bool> = Bindable(false)
     var tracks: Bindable<[Track]?> = Bindable(nil)
     var error: Bindable<CommonError?> = Bindable(nil)
-
-
+        
     private var httpClient: HTTPClient!
-
-
-    init(client: HTTPClient? = nil) {
-           self.httpClient = client ?? HTTPClient.shared
-    }
-       
-
     
-
+    init(client: HTTPClient? = nil) {
+        self.httpClient = client ?? HTTPClient.shared
+    }
+    
+    func getTrackList() {
+        isLoading.value = true
+        httpClient.dataTask(<#RequestProtocol#>) { [weak self] (result) in
+            guard let self = self else {
+                return
+            }
+            
+            self.isLoading.value = false
+            switch result {
+                
+            case .success(let data):
+                guard let data = data else {
+                    return
+                }
+                do {
+                    let tracks = try JSONDecoder().decode([Track].self, from: data)
+                    self.tracks.value = tracks
+                } catch {
+                    debugPrint("Unable to decode track List \(error)")
+                }
+            case .failure(let error):
+                self.error.value = CommonError(error.localizedDescription)
+                debugPrint("Error in fetching tracks \(error)")
+            }
+        }
+    }
+    
+    
+    
 }
