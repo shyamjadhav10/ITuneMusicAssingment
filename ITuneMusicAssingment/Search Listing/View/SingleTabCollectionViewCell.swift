3a948db297f5df0265e57b5a53d9dec457098788
Shyam Jadhav <shyamsundar@wakency.com>
2020-04-24 16:03:21 +0530

    [UI] - Added design for collection view cell name

--- ITuneMusicAssingment/Search Listing/View/SingleTabCollectionViewCell.swift
+++ ITuneMusicAssingment/Search Listing/View/SingleTabCollectionViewCell.swift
@@ -9,13 +9,32 @@
 import UIKit
 
 class SingleTabCollectionViewCell: UICollectionViewCell {
+    
+    
+    // MARK: - Outlets
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var yearAndTypeLabel: UILabel!
+    @IBOutlet weak var trackName: UILabel!
+    @IBOutlet weak var collectionName: UILabel!
+    @IBOutlet weak var artistName: UILabel!
+    
+    
+    // MARK: - Declarations
+
+    
+    
+    // MARK: - Default Methods
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        setupView()
     }
-
     
     
-    
+    // MARK: - Setup Methods
+    func setupView(){
+        
+    }
     
 }
