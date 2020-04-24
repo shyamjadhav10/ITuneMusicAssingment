65649361a7a1b873ce76a87b92e04acd1f14055d
Shyam Jadhav <shyamsundar@wakency.com>
2020-04-24 13:39:57 +0530

    [UI] - Added collection view cell

---
+++ ITuneMusicAssingment/Search Listing/View/SingleTabCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SingleTabCollectionViewCell.swift
+//  ITuneMusicAssingment
+//
+//  Created by Shyam Jadhav on 24/04/20.
+//  Copyright © 2020 Shyam Jadhav. All rights reserved.
+//
+
+import UIKit
+
+class SingleTabCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
