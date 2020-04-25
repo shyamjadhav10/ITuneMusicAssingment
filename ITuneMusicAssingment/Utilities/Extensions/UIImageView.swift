8b00c81b2dfe639ec51d2f616c78561c7a8e64f6
Shyam Jadhav <shyamsundar@wakency.com>
2020-04-25 18:45:07 +0530

    [UI] - WIP Image downloading async

---
+++ ITuneMusicAssingment/Utilities/Extensions/UIImageView.swift
@@ -0,0 +1,37 @@
+//
+//  UIImageView.swift
+//  ITuneMusicAssingment
+//
+//  Created by Shyam Jadhav on 25/04/20.
+//  Copyright © 2020 Shyam Jadhav. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+// MARK :- UIImageView
+extension UIImageView {
+    
+    // MARK :- API Calls
+    func fetchImages(pictureUrl : String){
+        
+        
+        guard !pictureUrl.isEmpty else { return  } // it will generate fatal issue if restaurant image is not present
+        let pictureKey = NSString(string: pictureUrl)
+        
+        self.image = nil // setting it to nil
+        
+        if let image = Constants.imageCache.object(forKey: pictureKey){
+            self.image = image
+            return
+        
+        }else {
+            
+        
+        }
+    }
+}
+
+
+
