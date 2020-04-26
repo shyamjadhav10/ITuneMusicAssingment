6d37e54c126b1ef6ff16010fa9a0075ec6bfca5c
Shyam Jadhav <shyamsundar@wakency.com>
2020-04-26 10:05:29 +0530

    [UI] - Now downloading images asynchronously

--- ITuneMusicAssingment/Utilities/Extensions/UIImageView.swift
+++ ITuneMusicAssingment/Utilities/Extensions/UIImageView.swift
@@ -16,7 +16,6 @@ extension UIImageView {
     // MARK :- API Calls
     func fetchImages(pictureUrl : String){
         
-        
         guard !pictureUrl.isEmpty else { return  } // it will generate fatal issue if restaurant image is not present
         let pictureKey = NSString(string: pictureUrl)
         
@@ -25,9 +24,30 @@ extension UIImageView {
         if let image = Constants.imageCache.object(forKey: pictureKey){
             self.image = image
             return
-        
+            
         }else {
-
+            HTTPClient.shared.dataTask(TrackAPI.getImage(url: pictureUrl)) { (result) in
+                
+                switch result {
+                case .success(let data):
+                    
+                    guard let data = data else {
+                        return
+                    }
+                    do {
+                        if let image = UIImage(data: data) {
+                            Constants.imageCache.setObject(image, forKey: pictureKey)
+                            
+                            // switching to main thread
+                            DispatchQueue.main.async {
+                                self.image = image
+                            }
+                        }
+                    }
+                case .failure(let error):
+                    debugPrint("Error in fetching images \(error)")
+                }
+            }
         }
     }
 }
